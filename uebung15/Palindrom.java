f12a7c03dd003d2df5bf1aa42b03e6d1b24c74c6
Aurianne <43066686+aurrriianne@users.noreply.github.com>
2020-05-01 15:42:59 +0200

    Update Palindrom.java

--- uebung15/Palindrom.java
+++ uebung15/Palindrom.java
@@ -7,24 +7,3 @@ public interface Palindrom
 {
     boolean istPalindrom(String wort);
 }
-public class PalindromRekursiv implements Palindrom {
-    @Override
-    boolean istPalindrom(String wort){
-        if (wort.length() <= 1)
-            return true;
-        else
-            return wort.charAt(0) == wort.charAt(wort.length() - 1) && 
-                        istPalindrom(wort.substring(1, wort.length() - 1));
-    }
-}
-public class PalindromIterativ implements Palindrom {
-    @Override
-    boolean istPalindrom(String wort){
-        for (int i = 0, j = wort.length()-1; i <= wort.length() / 2; i++, j--) {
-            if (wort.charAt(i) != wort.charAt(j))
-                return false;
-        }
-        return true;
-    }
-}
-

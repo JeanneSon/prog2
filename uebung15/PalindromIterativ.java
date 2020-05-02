695bd70485a37ba16e898b53bf123b7bed37da87
Aurianne <43066686+aurrriianne@users.noreply.github.com>
2020-05-02 10:20:38 +0200

    added Kommentar

--- uebung15/PalindromIterativ.java
+++ uebung15/PalindromIterativ.java
@@ -1,4 +1,16 @@
+/**
+ * die Klasse PalindromIterativ die die Klasse Palindrom implementiert
+ * 
+ * @author A. Venet und H. Schall
+ * @version 1.0
+ */
 public class PalindromIterativ implements Palindrom {
+    /**
+     * istPalindrom
+     *
+     * @param wort ein String nur mit buchstaben
+     * @return true wenn das wort ein Palindrom ist false sonst
+     */
     @Override
     public boolean istPalindrom(String wort){
         for (int i = 0, j = wort.length()-1; i <= wort.length() / 2; i++, j--) {

9baf017c73c6e968a4f4cb50f0fc670022785b65
Hanna <43119728+JeanneSon@users.noreply.github.com>
2020-05-15 19:05:07 +0200

    Update Divide.java

--- uebung16/Divide.java
+++ uebung16/Divide.java
@@ -2,8 +2,14 @@ public class Divide implements CrunchOperation
 {
     public static void crunch(float values[])
     {
+        int[] indexes = new int[];
+        for (int i = 1; i < values.length; i++) {
+            indexes[i] = i;
+        }
         float[] sortedValues = new float[values.length];
         System.arraycopy(values, 0, sortedValues, 0, 3);
+        sortedValues = insertionSort(sortedValues, indexes);
+        //here comes for-loop which does the work
     }
     
     
@@ -13,18 +19,21 @@ public class Divide implements CrunchOperation
      * @param tab zu sortierendes Int-Array
      * @return sortiertes Int-Array
      */
-    public static float[] insertionSort(float tab[])
+    public static float[] insertionSort(float tab[], int ind[])
     {
         int l = tab.length; 
         int temp, j;
         for (int i = 1; i < l; i++) {
             temp = tab[i];
+            temp2 = ind[i];
             j = i;
             while (j > 0 && tab[j-1] > temp) {
                 tab[j] = tab[j-1];
+                ind[j] = ind[j-1];
                 j--;
             }
             tab[j] = temp;
+            ind[j] = temp2;
         }
         return tab;
     }

869265399ca80fad589cb3e5567513db73d2785d
Hanna <43119728+JeanneSon@users.noreply.github.com>
2020-05-16 14:06:04 +0200

    added toString and comments to Average.java

--- uebung16/Average.java
+++ uebung16/Average.java
@@ -1,7 +1,16 @@
 public class Average implements CrunchOperation
 {
+  /**
+   * Konstruktor ist private, damit keine Objekte von der Klasse angelegt werden koennen
+   */
   private Average();
   
+  /**
+   * Bestimmt den Durchschnitt aller Werte im Array und 
+   * speichert den Durchschnittswert im Datenfeld mit dem groessten Wert. 
+   *
+   * @param values das Array, das veraendert wird (s.o.)
+   */
   public static void crunch(float values[])
   {
     int indexLargestVal = 0;
@@ -13,4 +22,15 @@ public class Average implements CrunchOperation
     }
     values[indexLargestVal] = sum / values.length;
   }
+  
+  /**
+   * gibt die Aufgabe der Klasse als String zurueck
+   *
+   * @return String
+   */
+   public static String toString()
+   {
+    return "Bestimmt den Durchschnitt aller Werte im Array und"
+           + "speichert den Durchschnittswert im Datenfeld mit dem groessten Wert.";
+   }   
 }

19604ffed92e39ffedce7c4710c5c2db76cbeb59
Aurianne <43066686+aurrriianne@users.noreply.github.com>
2020-05-16 17:57:49 +0200

    Update and rename Operator.java to Sum.java

---
+++ uebung16/Sum.java
@@ -0,0 +1,27 @@
+/**
+ * Sum
+ * 
+ * @author A. Venet u. H. Schall
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+class Sum implements CrunchOperation {
+  // Fehlermeldungen
+  private static final String MSG_ARRAY_LEER = "Array darf nicht leer sein!";
+  public void crunch (float values[]) 
+  {
+    int l = values.length;
+        check(l != 0, MSG_ARRAY_LEER);  
+        for(int i = 1; i < l; i++){
+            values[i] = values[i-1] + values[i];
+        }
+        return values;
+    }  
+  public String toString()
+  {
+    return "Summiert die Elemente des Arrays paarweise von links nach rechts D.h.: a[1] = a[0] + a[1]; a[2] =a[1] + a[2] usw.";
+  }
+  private static void check(boolean bedingung, String msg) {
+        if (!bedingung)
+            throw new IllegalArgumentException(msg);
+    }
+}

8287f9e693bc675d474cb56319cba20992b3d37d
Aurianne <43066686+aurrriianne@users.noreply.github.com>
2020-06-01 18:40:11 +0200

    update conditionate

--- uebung17/Conditionate.java
+++ uebung17/Conditionate.java
@@ -10,18 +10,36 @@ public interface Conditionate extends MyFunction
 {
     
     /**
-     * Méthode conditionateInput
+     * conditionateInput Methode
      *
-     * @return La valeur de retour
+     * @return ein lambda ausdruck
      */
-    int conditionateInput(Predicate predic);
+    public default MyFunction conditionateInput(Predicate<Integer> pred){
+        MyFunction predicate_lambda = (int n) -> {
+            if (pred.test(n)) {
+                return this.apply(n);
+            }else {
+                return 0;
+            }
+        };
+        return predicate_lambda;
+    }
     
     /**
-     * Méthode conditionateOutput
+     * conditionateOutput Methode
      *
-     * @return La valeur de retour
+     * @return ein lambda ausdruck
      */
-    int conditionateOutput(Predicate predic);
+    public default MyFunction conditionateOutput(Predicate<Integer> pred){
+        MyFunction predicate_lambda = (int n) -> {
+            if (pred.test(this.apply(n))) {
+                return this.apply(n);
+            }else {
+                return 0;
+            }
+        };
+        return predicate_lambda;
+    };
     
     Predicate<Integer> even = i -> i % 2 == 0;
     //System.out.println(even.test(33)) -> False

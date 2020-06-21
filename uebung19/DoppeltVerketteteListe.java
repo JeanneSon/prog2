d4b4db23af15450cfdeb18d34a4b263d3179ae1f
Aurianne <43066686+aurrriianne@users.noreply.github.com>
2020-06-21 17:18:55 +0200

    add DoppeltVerketteteListe

---
+++ uebung19/DoppeltVerketteteListe.java
@@ -0,0 +1,129 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class DoppeltVerketteteListe<E> implements List<E> {
+
+	@Override
+	public boolean add(E arg0) {
+		return false;
+	}
+
+	@Override
+	public void add(int arg0, E arg1) {
+		
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends E> arg0) {
+		return false;
+	}
+
+	@Override
+	public boolean addAll(int arg0, Collection<? extends E> arg1) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void clear() {
+		
+	}
+
+	@Override
+	public boolean contains(Object arg0) {
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> arg0) {
+	    throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int indexOf(Object arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int lastIndexOf(Object arg0) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public ListIterator<E> listIterator() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public ListIterator<E> listIterator(int arg0) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean remove(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> arg0) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> arg0) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int size() {
+		return 0;
+	}
+
+	@Override
+	public List<E> subList(int arg0, int arg1) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object[] toArray() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public <T> T[] toArray(T[] arg0) {
+		return null;
+	}
+
+	@Override
+	public E get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E set(int index, E element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

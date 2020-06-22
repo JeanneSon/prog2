bd11c74e9c9ac2a41536436c12852e3e630eb0e6
Aurianne <43066686+aurrriianne@users.noreply.github.com>
2020-06-22 15:26:20 +0200

    Update Testklasse.java

--- uebung19/Testklasse.java
+++ uebung19/Testklasse.java
@@ -6,22 +6,35 @@ import java.util.*;
 * @author A. Venet und H. Schall
 * @version 1.0
 */
-public class Testklasse
+public class Testklasse extends DoppeltVerketteteListe
 {
     //Attribute
     private Scanner input = new Scanner(System.in);
-    
+
     //Klassenkonstanten
     private static final int ENDE = 0;
+    private static final int SIZE = 1;
+    private static final int CONTAINS = 2;
+    private static final int IS_EMPTY = 3;
+    private static final int TO_ARRAY = 4;
+    private static final int ADD = 5;
+    private static final int REMOVE_O = 6;
+    private static final int ADD_ALL = 7;
+    private static final int CLEAR = 8;
+    private static final int GET = 9;
+    private static final int SET = 10;
+    private static final int ADD_INDEX = 11;
+    private static final int REMOVE_INDEX = 12;
+    private static final int INDEX_OF = 13;
 
     /**
     *Hauptschleife des Testprogramms
     */
     public void start() {
         System.out.println("HERZLICH WILLKOMMEN");
-        
+
         int auswahl = -1;
-    
+
         while (auswahl != ENDE) {
             try {
                 auswahl = einlesenAuswahl();
@@ -37,7 +50,7 @@ public class Testklasse
             }
         }
     }
-    
+
     /**
     * einlesenAuswahl Methode
     * Menue ausgeben und Auswahl einlesen
@@ -47,24 +60,73 @@ public class Testklasse
     private int einlesenAuswahl() {
         int auswahl;
         System.out.print(
+
+        SIZE            + ": Gibt die Größe der Doppelt verkettetetn Liste zurück; \n" +
+        IS_EMPTY        + ": Prüft ob DVL leer ist\n"+
+        CONTAINS        + ": Prüft ob bestimmtes Object in DVL enthalten ist\n"+
+        TO_ARRAY        + ": Wandelt DVL in Array um\n"+
+        ADD             + ": Fügt Node am Ende der DVL an\n"+
+        REMOVE_O        + ": Entfernt ein bestimmtes Objekt\n"+
+        ADD_ALL         + ": Alles ans Ende der Liste anfügen\n"+
+        CLEAR           + ": Löscht alles\n"+
+        GET             + ": Bestimmten Wert ausgeben\n"+
+        SET             + ": Auf bestimmten Wert setzen\n"+
+        ADD_INDEX       + ": Node an bestimmter Stelle einfügen\n"+
+        REMOVE_INDEX    + ": Node an bestimmter Stelle löschen\n"+
+        INDEX_OF        + ": Index eines bestimmten Wertes ausgeben\n"+
         ENDE            + ": Programm beenden; \nAuswahl:\t");
-        
+
         auswahl = input.nextInt();
         input.nextLine();
         return auswahl;
     }
-    
+
     private void ausfuehrenAuswahl(int auswahl) {
         switch (auswahl) {
-            case ENDE:
-                System.out.println("Programmende");
+            case SIZE:
+                size();
+                break;
+            case IS_EMPTY:
+                isEmpty();
+                break;
+            case CONTAINS:
+                contains();
+                break;
+            case TO_ARRAY:
+                toArray();
+                break;
+            case ADD:
+                add();
+                break;
+            case REMOVE_O:
+                remove();
+                break;
+            case ADD_ALL:
+                addAll();
+                break;
+            case CLEAR:
+                clear();
+                break;
+            case GET:
+                get();
+                break;
+            case SET:
+                set();
+                break;
+            case ADD_INDEX:
+                add();
+                break;
+            case REMOVE_INDEX:
+                remove();
+                break;
+            case INDEX_OF:
+                indexOf();
                 break;
             default:
                 System.out.println("Falsche Auswahl!");
         }
         System.out.println();
     }
-
     /**
      * Main-Methode zum Erzeugen des ArtikelDialog-Objekts und zum Anstarten der Testschleife
      */

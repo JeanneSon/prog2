b552ba92d46a8abdc52c7be0d403a84fbd57aae2
Aurianne <43066686+aurrriianne@users.noreply.github.com>
2020-06-27 19:50:12 +0200

    add copy part 1

---
+++ uebung20/ueb20_1/Consumer.java
@@ -0,0 +1,44 @@
+/**
+ * Beschreiben Sie hier die Klasse Consumer.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Consumer
+{
+    
+    //private Map enthaelt Collection; // -> ArrayList // -> TreeMap;
+
+    /**
+     * Konstruktor für Objekte der Klasse Consumer
+     */
+    public Consumer()
+    {
+        // Instanzvariable initialisieren
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public void consume(int i)
+    {
+        //Quersumme
+        //stecke Quersumme und Time in Map
+    }
+    
+    private void neuerEintrag (int i, long miliseconds) {
+        //quersumme schon als schlüssel in treeMap?
+        //falls ja -> miliseconds hinzufügen zur Arraylist von dieser quersumme
+        //falls nein -> neuer eintrag mit quersumme als schlüssel und arraylist als wert; arraylist enthält miliseconds
+    }
+    
+    //getCrossTotalsDescending -> arrayList
+    
+    ////getCrossTotalsAscending -> ArrayList
+    
+    //getTimestampsForResult 
+    //gibt collection zurück ArrayList
+}

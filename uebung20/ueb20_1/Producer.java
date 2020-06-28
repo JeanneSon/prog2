0e455388fcc652ec057c9aff8d5dcbe38d5ba917
Aurianne <43066686+aurrriianne@users.noreply.github.com>
2020-06-28 11:43:57 +0200

    update random producer

--- uebung20/ueb20_1/Producer.java
+++ uebung20/ueb20_1/Producer.java
@@ -1,31 +1,28 @@
+import java.util.Random;
 /**
  * Beschreiben Sie hier die Klasse Producer.
  * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
+ * @author VENET Aurianne & SCHALL Hanna
+ * @version 1.0
  */
 public class Producer
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
+   
     /**
      * Konstruktor für Objekte der Klasse Producer
      */
     public Producer()
     {
-        // Instanzvariable initialisieren
-        x = 0;
+        
     }
 
     /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
      * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
      */
     public int produce()
     {
-        return 6;
+        Random random = new Random();
+        int number = random.nextInt(1000);
+        return number;
     }
 }
